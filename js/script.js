db657330e8413e7ed2fe02398b41a565eb7bbda3
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-27 15:15:28 -0700

    making sure things are saved

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var anchor = document.querySelector('a');
+console.log(anchor);
+var allanchors = document.querySelectorAll('a');
+console.log(allanchors);
+
+allanchors[1].textContent = 'This text is set from JS using DOM manipulation.';
+allanchors[1].href ='https://www.google.com';
+allanchors[1].target = '_blank';
+
+var para1 = document.getElementById('para-1');
+console.log(para1);
+var para1usingselector = document.querySelector('#para-1');
+console.log(para1usingselector);
+para1.style.color ='blue';
+var btnusingselector = document.querySelector('.button');
+console.log(btnusingselector);
+btn[0].style.padding ='10px';
+btn[0].style.fontsize = '22px';
