fec41b01525591ef326142adb1d2670e9735caa2
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-27 20:36:11 -0700

    added color, fontsize and margin to rev man.2

--- js/review-dom-manipulation-1.js
+++ js/review-dom-manipulation-1.js
@@ -1,4 +1,3 @@
 
 var para = document.querySelector('#para');
 console.log(para);
-para.style.color = "green";
---
+++ js/review-dom-manipulation-2.js
@@ -0,0 +1,5 @@
+var para2 = document.querySelector('#para2');
+console.log(para2);
+para2.style.color = "green";
+para2.style.fontsize = '20px';
+para2.style.margin = "60px";
