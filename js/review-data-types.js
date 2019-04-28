b5eb0467a41626eebe43be4dbfa27e08da8fd3cd
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-27 17:53:38 -0700

    first assignment

---
+++ js/review-data-types.js
@@ -0,0 +1,31 @@
+var str1 ='What day is it.';
+console.log(str1);
+
+var str2 = 'It\'s hot';
+console.log(str2);
+
+var bol = true;
+var bol2 = false;
+
+
+var arr1 = ['hot', 'cold','fresh'];
+console.log(arr1);
+console.log(arr1[0]);
+console.log(arr1[2]);
+
+var arr2 = [' ', '20,30,40'];
+console.log(arr2);
+console.log(arr2[1]);
+console.log(arr2[0]);
+
+
+var obj = {first:'Ana', last:'Uyehara'};
+console.log(obj.first,obj.last);
+var tempstr = '';
+var obj =  tempstr.concat(obj.last,' ',obj.first);
+console.log(obj);
+
+
+
+
+
