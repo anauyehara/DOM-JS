bd36e29335ccfbb3cb4915d1ed1ba4e6999da521
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-29 18:25:43 -0700

    finsihed

--- js/review-dom-manipulation-3.js
+++ js/review-dom-manipulation-3.js
@@ -1,10 +1,15 @@
-var class = document.getElementsByClassName(class);
-console.log(class[0]);
-.class{
-    display: inline-block;
-    height: '50px';
+var div= document.getElementsByClassName('content');
+div [0].style.height = '600px';
+div [0].display= 'inline-block';
+console.log(div);
 
-}
+
+
+
+
+    
+  
+  
 
 
 
