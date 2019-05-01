607929afa3bbeefe9e71b46c3e06e64c3df262ad
Ana uyehara <ana.uyehara0@gmail.com>
2019-04-30 19:32:41 -0700

    more to come

---
+++ js/operators.js
@@ -0,0 +1,7 @@
+// 2 variables x & y that store numbers & assign some valuse
+//check if x is greater than 18 & y is greater than 21
+//then display "Horray!"
+//else if x is greater than 14 & less than 18
+//
+
+var 
