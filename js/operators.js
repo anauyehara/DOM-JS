8a04b414fdf2730bede7b53c722708115f99e723
Ana uyehara <ana.uyehara0@gmail.com>
2019-05-02 19:02:03 -0700

    aged out

--- js/operators.js
+++ js/operators.js
@@ -2,7 +2,33 @@
 //check if x is greater than 18 & y is greater than 21
 //then display "Horray!"
 //else if x is greater than 14 & less than 18
-//
+//var age  stores numbers
 
-var age = 'A number';
-console.log(age.typeof);
+//var age = 21,
+//  accompaniedByAdult = true;
+
+var age = prompt ('Enter your age');
+
+if (age >= 18 && age < 21)
+{
+var accompaniedByAdult =confirm('Are you accompanied by an someone over 21');
+
+console.log(age, accompaniedByAdult);
+}
+//  if (age>= 18 && age < 21){
+//      if (accompaniedByAdult == true){
+//          console.log('Allowed');
+//      }else {
+//          console.log('Denied');
+//      }
+//      accompaniedByAdult ? console.log('Allowed'): console.log('Denied');
+//  } else if (age >= 21){
+//      console.log('You are free');
+//  } else {
+//      console.log('age is not between 18-21');
+//      console.log('Try again next year');
+//  }
+
+//  //ternary operator:
+ //accompaniedByAdult ? console.log('Allowed') : console.log ('Denied');
+
