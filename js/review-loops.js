3cb865556457ff5e813d4c76ce0e3d332b034d60
Ana uyehara <ana.uyehara0@gmail.com>
2019-05-06 18:39:05 -0700

    review loops

---
+++ js/review-loops.js
@@ -0,0 +1,47 @@
+//var users = {
+  //  name: 'Ana',
+    //hp: 50,
+ 
+//};
+//console.log(user);
+
+var users = [];
+//users.push(user);
+//console.log(users);
+
+var userCount = 5;
+
+//var user = {};
+// diy: ad validation on these prompts
+//user.name = prompt( 'Enter User name:');
+//user.hp = prompt('Enter User hp:');
+//console.log(user);
+
+//while (users.lenght < userCount){
+  //  var user = {};
+    // diy: ad validation on these prompts
+   // user.name = prompt( 'Enter User name:');
+    //user.hp = prompt('Enter User hp:');
+    //console.log(user);
+   // users.push(user);
+
+//}
+
+for (var i = 0; i < userCount; i++){
+  var user = {};
+  // diy: ad validation on these prompts
+  user.name = prompt( 'Enter User name:');
+  user.hp = prompt('Enter User hp:');
+  console.log(user);
+  users.push(user);
+
+}
+console.log(users);
+//output each user to console.log
+
+console.log();
+
+
+
+
+
